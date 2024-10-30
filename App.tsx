afc71794770ce1e1b3ddb73753b82a3a9103d175
samsoorsamander <samsoorsamander@gmail.com>
2024-10-30 10:54:40 -0700

    add a shoping List item

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PixelRatio } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! </Text>
+      <View style ={styles.itemContainer}>
+      <Text style={styles.itemText}> Coffee </Text>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
     justifyContent: 'center',
   },
+  itemContainer: {
+    paddingHorizontal:8,
+    paddingVertical: 16,
+    borderBottomWidth:1,
+    borderBottomColor:"#1a759f"
+  },
+  itemText: {
+    fontSize: 18,
+     fontWeight:"200"}
 });
