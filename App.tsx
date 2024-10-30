b5ddee5bbfa0a015021564241f037550863b38ff
samsoorsamander <samsoorsamander@gmail.com>
2024-10-30 11:25:20 -0700

    add a delete button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, PixelRatio } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity,Alert } from 'react-native';
+import { theme } from './theme';
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Are you sure you want to delete this?",
+    "It will be gone for good",
+    [
+      {
+        text:"Yes",
+        onPress:() =>console.log("deleting"),
+        style:'destructive'
+      },
+      {
+        text:"Cancel",
+        style:'cancel'
+      }
+    ]
+    );
+  }
   return (
     <View style={styles.container}>
+     
       <View style ={styles.itemContainer}>
       <Text style={styles.itemText}> Coffee </Text>
+      <TouchableOpacity
+      style= {styles.button}
+       onPress={handleDelete} 
+       activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
 
       </View>
       <StatusBar style="auto" />
@@ -23,9 +46,24 @@ const styles = StyleSheet.create({
     paddingHorizontal:8,
     paddingVertical: 16,
     borderBottomWidth:1,
-    borderBottomColor:"#1a759f"
+    borderBottomColor:"#1a759f",
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent: 'space-between',
   },
   itemText: {
     fontSize: 18,
-     fontWeight:"200"}
+     fontWeight:"200"
+    },
+    button: {
+      backgroundColor:theme.colorBalck,
+      padding:8,
+      borderRadius:6,
+    },
+    buttonText: {
+      color:theme.colorWhite,
+      fontWeight:'bold',
+      textTransform:'uppercase',
+      letterSpacing:1,
+    }
 });
--- theme.ts
+++ theme.ts
@@ -1,4 +1,5 @@
 export const theme ={
     colorCerulean:"#1a759f",
     colorWhite:"#fff",
+    colorBalck:"#000",
 }
